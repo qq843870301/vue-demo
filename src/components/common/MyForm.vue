5e44329d9121cc0dea703115af324df6295ebe8e
chenm <chenmin@hotmail.de>
2019-11-19 18:55:26 +0800

    form

--- src/components/common/MyForm.vue
+++ src/components/common/MyForm.vue
@@ -1,57 +1,119 @@
 <!-- 搜索表单 -->
 <template>
   <div class="ces-search">
-    <el-form :size="size" :inline='false' :label-width="labelWidth">
+    <el-form :size="size" :inline="false" :label-width="labelWidth">
       <el-form-item v-for="item in searchForm" :label="item.label" :key="item.prop">
         <!-- 输入框 -->
-        <el-input v-if="item.type==='Input'" v-model="searchData[item.prop]" size="small"></el-input>
+        <el-input
+          v-if="item.type==='Input'"
+          v-model="searchData[item.prop]"
+          size="small"
+          :placeholder="item.placeholder"
+          :disabled="item.disabled"
+          :style="{width:item.width}"
+        ></el-input>
+        <!-- 文本域 -->
+        <el-input
+          v-if="item.type==='textarea'"
+          type="textarea"
+          :disabled="item.disabled && item.disabled(editData)"
+          @change="item.change && item.change(editData[item.prop])"
+          v-model="editData[item.prop]"
+          :style="{width:item.width}"
+        ></el-input>
         <!-- 下拉框 -->
         <el-select
           v-if="item.type==='Select'"
           v-model="searchData[item.prop]"
           size="small"
+          :placeholder="item.placeholder"
+          :disabled="item.disabled"
+          :style="{width:item.width}"
           @change="item.change(searchData[item.prop])"
         >
           <el-option v-for="op in item.options" :label="op.label" :value="op.value" :key="op.value"></el-option>
         </el-select>
         <!-- 单选 -->
-        <el-radio-group v-if="item.type==='Radio'" v-model="searchData[item.prop]">
+        <el-radio-group
+          v-if="item.type==='Radio'"
+          v-model="searchData[item.prop]"
+          :disabled="item.disabled"
+        >
           <el-radio v-for="ra in item.radios" :label="ra.value" :key="ra.value">{{ra.label}}</el-radio>
         </el-radio-group>
         <!-- 单选按钮 -->
         <el-radio-group
           v-if="item.type==='RadioButton'"
           v-model="searchData[item.prop]"
+          :disabled="item.disabled"
           @change="item.change && item.change(searchData[item.prop])"
         >
           <el-radio-button v-for="ra in item.radios" :label="ra.value" :key="ra.value">{{ra.label}}</el-radio-button>
         </el-radio-group>
         <!-- 复选框 -->
-        <el-checkbox-group v-if="item.type==='Checkbox'" v-model="searchData[item.prop]">
+        <el-checkbox-group
+          v-if="item.type==='Checkbox'"
+          v-model="searchData[item.prop]"
+          :disabled="item.disabled"
+        >
           <el-checkbox v-for="ch in item.checkboxs" :label="ch.value" :key="ch.value">{{ch.label}}</el-checkbox>
         </el-checkbox-group>
         <!-- 日期 -->
-        <el-date-picker v-if="item.type==='Date'" v-model="searchData[item.prop]"></el-date-picker>
+        <el-date-picker
+          v-if="item.type==='Date'"
+          v-model="searchData[item.prop]"
+          :placeholder="item.placeholder"
+          :disabled="item.disabled"
+          :style="{width:item.width}"
+        ></el-date-picker>
         <!-- 时间 -->
-        <el-time-select v-if="item.type==='Time'" v-model="searchData[item.prop]" type></el-time-select>
+        <el-time-select
+          v-if="item.type==='Time'"
+          v-model="searchData[item.prop]"
+          type
+          :placeholder="item.placeholder"
+          :disabled="item.disabled"
+          :style="{width:item.width}"
+        ></el-time-select>
         <!-- 日期时间 -->
         <el-date-picker
           v-if="item.type==='DateTime'"
           type="datetime"
           v-model="searchData[item.prop]"
+          :placeholder="item.placeholder"
           :disabled="item.disable && item.disable(searchData[item.prop])"
+          :style="{width:item.width}"
         ></el-date-picker>
         <!-- 滑块 -->
-        <!-- <el-slider v-if="item.type==='Slider'" v-model="searchData[item.prop]"></el-slider> -->
+        <el-slider
+          v-if="item.type==='Slider'"
+          v-model="searchData[item.prop]"
+          :disabled="item.disabled"
+        ></el-slider>
         <!-- 开关 -->
-        <el-switch v-if="item.type==='Switch'" v-model="searchData[item.prop]"></el-switch>
+        <el-switch
+          v-if="item.type==='Switch'"
+          v-model="searchData[item.prop]"
+          :disabled="item.disabled"
+        ></el-switch>
         <!-- 按钮 -->
-        <el-button v-if="item.type==='Button'" :type="item.BtnType" :size="item.size || size" @click="item.handle()">{{item.label}}</el-button>
+        <el-button
+          v-if="item.type==='Button'"
+          :type="item.BtnType"
+          :size="item.size || size"
+          @click="item.handle()"
+          :disabled="item.disabled"
+        >{{item.text}}</el-button>
       </el-form-item>
     </el-form>
-    <el-form :inline='true' v-if="isHandle" :align="align">
+    <el-form :inline="true" v-if="isHandle" :align="align">
       <el-form-item v-for="item in searchHandle" :key="item.label">
-        <el-button :type="item.type" :size="item.size || size" @click="item.handle()">{{item.label}}</el-button>
+        <el-button
+          :type="item.type"
+          :size="item.size || size"
+          @click="item.handle()"
+          :disabled="item.disabled"
+        >{{item.label}}</el-button>
       </el-form-item>
     </el-form>
   </div>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -30,9 +30,13 @@ export default {
       formData: {
         searchData: {
           name: null,
-          age: null,
+          id: null,
+          pwd: null,
+          address: null,
+          phone: null,
+          department_id: null,
+          role: null,
           sex: null,
-          date: "",
           interst: []
         },
         searchForm: [
@@ -40,52 +44,100 @@ export default {
             type: "Input",
             label: "姓名",
             prop: "name",
-            width: "180px",
+            width: "200px",
             placeholder: "请输入姓名"
           },
           {
             type: "Input",
-            label: "年龄",
-            prop: "age",
-            width: "180px",
-            placeholder: "请输入年龄"
+            label: "编号",
+            prop: "id",
+            width: "200px",
+            disabled: true
           },
           {
-            type: "Date",
-            label: "日期",
-            prop: "date",
-            width: "180px",
-            placeholder: "请选择日期"
+            type: "Input",
+            label: "密码",
+            prop: "pwd",
+            width: "200px",
+            disabled: true
+          },
+          {
+            type: "Select",
+            label: "部门",
+            prop: "address",
+            width: "200px",
+            options: [{ label: "男", value: "M" }, { label: "女", value: "F" }],
+            props: sexProps,
+            change: row => "",
+            placeholder: "请选择部门"
+          },
+          {
+            type: "Input",
+            label: "联系方式",
+            prop: "phone",
+            width: "200px",
+            placeholder: "请输入联系方式"
+          },
+          {
+            type: "Input",
+            label: "地址",
+            prop: "department_id",
+            width: "200px",
+            placeholder: "请输入地址"
+          },
+          {
+            type: "Select",
+            label: "权限",
+            prop: "role",
+            width: "200px",
+            options: [
+              { label: "数据管理员", value: "2" },
+              { label: "会议管理者", value: "3" },
+              { label: "普通用户", value: "4" }
+            ],
+            props: sexProps,
+            change: row => "",
+            placeholder: "请选择权限"
           },
           {
             type: "Select",
             label: "性别",
             prop: "sex",
-            width: "120px",
+            width: "200px",
             options: sexs,
             props: sexProps,
             change: row => "",
             placeholder: "请选择性别"
           },
           {
-            type: "Checkbox",
-            label: "爱好",
-            prop: "interst",
-            checkboxs: intersts,
-            props: interstProps
+            type: "Button",
+            BtnType: "primary",
+            size: "mini",
+            label: "人脸录制",
+            text: "开始录制",
+            handle: () => (this.dialogTableVisible2 = true)
           },
           {
             type: "Button",
             BtnType: "primary",
             size: "mini",
-            label: "开启视频",
-            handle: () => console.log(1)
+            label: "声纹录制",
+            text: "开始录制",
+            handle: () => (this.dialogTableVisible = true)
+          },
+          {
+            type: "Button",
+            BtnType: "primary",
+            size: "mini",
+            label: "语音录制",
+            text: "开始录制",
+            handle: () => (this.dialogTableVisible1 = true)
           }
         ],
         //按钮
         searchHandle: [
           {
-            label: "查询",
+            label: "提交",
             type: "primary",
             size: "mini",
             handle: () => this.searchHandleForm()
